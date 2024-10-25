a40aa986e371f83723528992249697f1967fae5c
Ale Crismani <ale.crismani@automattic.com>
2024-10-25 12:44:14 +0000

    More spectre keybindings

--- lua/kickstart/plugins/spectre.lua
+++ lua/kickstart/plugins/spectre.lua
@@ -1,8 +1,17 @@
 return {
   'nvim-pack/nvim-spectre',
   config = function(opts)
-    vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
-      desc = 'Toggle [S]pectre',
+    vim.keymap.set('n', '<leader>St', '<cmd>lua require("spectre").toggle()<CR>', {
+      desc = '[S]pectre [Toggle]',
+    })
+    vim.keymap.set('n', '<leader>Sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+      desc = '[S]pectre search current [w]ord',
+    })
+    vim.keymap.set('v', '<leader>Sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+      desc = '[S]pectre search current word',
+    })
+    vim.keymap.set('n', '<leader>Sf', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+      desc = '[S]pectre search on current [f]ile',
     })
   end,
 }
