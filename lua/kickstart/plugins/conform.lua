f4b0dc046b44604fe21063ccd79d405737d035ab
Ale Crismani <ale.crismani@automattic.com>
2024-11-03 23:18:03 +0000

    Format .js and .ts files wuth eslint_d

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -27,7 +27,7 @@ return {
           lsp_format_opt = 'fallback'
         end
         return {
-          timeout_ms = 500,
+          timeout_ms = 20000,
           lsp_format = lsp_format_opt,
         }
       end,
@@ -37,7 +37,8 @@ return {
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'eslint_d', stop_after_first = true },
+        typescript = { 'eslint_d', stop_after_first = true },
       },
     },
   },
