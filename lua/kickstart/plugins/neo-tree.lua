fffa984c7787871f67471708aae67e90a13dc081
Alessandro Crismani <alessandro@Mac.home>
2025-01-16 14:05:37 +0100

    Add bindings for copying file paths in neo-tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -13,71 +13,117 @@ return {
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
-  opts = {
-    close_if_last_window = true,
-    sources = {
-      'filesystem',
-      -- 'buffers',
-      -- 'git_status',
-      -- "document_symbols",
-    },
-    source_selector = {
-      winbar = false,                        -- toggle to show selector on winbar
-      statusline = false,                    -- toggle to show selector on statusline
-      show_scrolled_off_parent_node = false, -- this will replace the tabs with the parent path
-      -- of the top visible node when scrolled down.
+  opts = function()
+    return {
+      close_if_last_window = true,
       sources = {
-        { source = 'filesystem' },
-        -- { source = 'buffers' },
-        -- { source = 'git_status' },
+        'filesystem',
+        -- 'buffers',
+        -- 'git_status',
+        -- "document_symbols",
       },
-      content_layout = 'center', -- only with `tabs_layout` = "equal", "focus"
-      --                start  : |/ 󰓩 bufname     \/...
-      --                end    : |/     󰓩 bufname \/...
-      --                center : |/   󰓩 bufname   \/...
-      tabs_layout = 'equal', -- start, end, center, equal, focus
-      --             start  : |/  a  \/  b  \/  c  \            |
-      --             end    : |            /  a  \/  b  \/  c  \|
-      --             center : |      /  a  \/  b  \/  c  \      |
-      --             equal  : |/    a    \/    b    \/    c    \|
-      --             active : |/  focused tab    \/  b  \/  c  \|
-      truncation_character = '…', -- character to use when truncating the tab label
-      tabs_min_width = nil, -- nil | int: if int padding is added based on `content_layout`
-      tabs_max_width = nil, -- this will truncate text even if `text_trunc_to_fit = false`
-      padding = 0, -- can be int or table
-      -- padding = { left = 2, right = 0 },
-      -- separator = "▕", -- can be string or table, see below
-      separator = { left = '▏', right = '▕' },
-      -- separator = { left = "/", right = "\\", override = nil },     -- |/  a  \/  b  \/  c  \...
-      -- separator = { left = "/", right = "\\", override = "right" }, -- |/  a  \  b  \  c  \...
-      -- separator = { left = "/", right = "\\", override = "left" },  -- |/  a  /  b  /  c  /...
-      -- separator = { left = "/", right = "\\", override = "active" },-- |/  a  / b:active \  c  \...
-      -- separator = "|",                                              -- ||  a  |  b  |  c  |...
-      separator_active = nil, -- set separators around the active tab. nil falls back to `source_selector.separator`
-      show_separator_on_edge = false,
-      --                       true  : |/    a    \/    b    \/    c    \|
-      --                       false : |     a    \/    b    \/    c     |
-      highlight_tab = 'NeoTreeTabInactive',
-      highlight_tab_active = 'NeoTreeTabActive',
-      highlight_background = 'NeoTreeTabInactive',
-      highlight_separator = 'NeoTreeTabSeparatorInactive',
-      highlight_separator_active = 'NeoTreeTabSeparatorActive',
-    },
-    window = {
-      mappings = {
-        ['Z'] = 'expand_all_nodes',
-      },
-    },
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        hide_dotfiles = false,
+      source_selector = {
+        winbar = false,                        -- toggle to show selector on winbar
+        statusline = false,                    -- toggle to show selector on statusline
+        show_scrolled_off_parent_node = false, -- this will replace the tabs with the parent path
+        -- of the top visible node when scrolled down.
+        sources = {
+          { source = 'filesystem' },
+          -- { source = 'buffers' },
+          -- { source = 'git_status' },
+        },
+        content_layout = 'center', -- only with `tabs_layout` = "equal", "focus"
+        --                start  : |/ 󰓩 bufname     \/...
+        --                end    : |/     󰓩 bufname \/...
+        --                center : |/   󰓩 bufname   \/...
+        tabs_layout = 'equal', -- start, end, center, equal, focus
+        --             start  : |/  a  \/  b  \/  c  \            |
+        --             end    : |            /  a  \/  b  \/  c  \|
+        --             center : |      /  a  \/  b  \/  c  \      |
+        --             equal  : |/    a    \/    b    \/    c    \|
+        --             active : |/  focused tab    \/  b  \/  c  \|
+        truncation_character = '…', -- character to use when truncating the tab label
+        tabs_min_width = nil, -- nil | int: if int padding is added based on `content_layout`
+        tabs_max_width = nil, -- this will truncate text even if `text_trunc_to_fit = false`
+        padding = 0, -- can be int or table
+        -- padding = { left = 2, right = 0 },
+        -- separator = "▕", -- can be string or table, see below
+        separator = { left = '▏', right = '▕' },
+        -- separator = { left = "/", right = "\\", override = nil },     -- |/  a  \/  b  \/  c  \...
+        -- separator = { left = "/", right = "\\", override = "right" }, -- |/  a  \  b  \  c  \...
+        -- separator = { left = "/", right = "\\", override = "left" },  -- |/  a  /  b  /  c  /...
+        -- separator = { left = "/", right = "\\", override = "active" },-- |/  a  / b:active \  c  \...
+        -- separator = "|",                                              -- ||  a  |  b  |  c  |...
+        separator_active = nil, -- set separators around the active tab. nil falls back to `source_selector.separator`
+        show_separator_on_edge = false,
+        --                       true  : |/    a    \/    b    \/    c    \|
+        --                       false : |     a    \/    b    \/    c     |
+        highlight_tab = 'NeoTreeTabInactive',
+        highlight_tab_active = 'NeoTreeTabActive',
+        highlight_background = 'NeoTreeTabInactive',
+        highlight_separator = 'NeoTreeTabSeparatorInactive',
+        highlight_separator_active = 'NeoTreeTabSeparatorActive',
       },
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ['Z'] = 'expand_all_nodes',
+          ['F'] = "find_in_dir",
+          ['Y'] = "copy_selector",
         },
       },
-    },
-  },
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+        },
+        window = {
+          mappings = {
+            ['\\'] = 'close_window',
+          },
+        },
+      },
+      commands = {
+        find_in_dir = function(state)
+          local node = state.tree:get_node()
+          local path = node:get_id()
+          require("telescope.builtin").find_files {
+            cwd = node.type == "directory" and path or vim.fn.fnamemodify(path, ":h"),
+          }
+        end,
+        copy_selector = function(state)
+          local node = state.tree:get_node()
+          local filepath = node:get_id()
+          local filename = node.name
+          local modify = vim.fn.fnamemodify
+
+          local vals = {
+            ["BASENAME"] = modify(filename, ":r"),
+            ["EXTENSION"] = modify(filename, ":e"),
+            ["FILENAME"] = filename,
+            ["PATH (CWD)"] = modify(filepath, ":."),
+            ["PATH (HOME)"] = modify(filepath, ":~"),
+            ["PATH"] = filepath,
+            ["URI"] = vim.uri_from_fname(filepath),
+          }
+
+          local options = vim.tbl_filter(function(val) return vals[val] ~= "" end, vim.tbl_keys(vals))
+          if vim.tbl_isempty(options) then
+            vim.notify("No values to copy", vim.log.levels.WARN)
+            return
+          end
+          table.sort(options)
+          vim.ui.select(options, {
+            prompt = "Choose to copy to clipboard:",
+            format_item = function(item) return ("%s: %s"):format(item, vals[item]) end,
+          }, function(choice)
+            local result = vals[choice]
+            if result then
+              vim.notify(("Copied: `%s`"):format(result))
+              vim.fn.setreg("+", result)
+            end
+          end)
+        end
+      },
+    }
+  end
 }
