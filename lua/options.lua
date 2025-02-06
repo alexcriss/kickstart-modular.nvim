2cfd3db4efada701c9f0512785fa048695a77a74
Alessandro Crismani <alessandro@Mac.home>
2025-02-06 12:44:24 +0100

    Support clipboard remotely

--- lua/options.lua
+++ lua/options.lua
@@ -21,6 +21,27 @@ vim.opt.showmode = false
 --  See `:help 'clipboard'`
 vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
+
+  local function paste()
+    return {
+      vim.split(vim.fn.getreg '', '\n'),
+      vim.fn.getregtype '',
+    }
+  end
+
+  if vim.env.SSH_TTY then
+    vim.g.clipboard = {
+      name = 'OSC 52',
+      copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+        ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+      },
+      paste = {
+        ['+'] = paste,
+        ['*'] = paste,
+      },
+    }
+  end
 end)
 
 -- Enable break indent
