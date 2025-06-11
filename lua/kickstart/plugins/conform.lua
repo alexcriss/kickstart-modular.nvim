146755c9bbdae4bf2e69c11196ef56e5e2eea1f7
Alessandro Crismani <alessandro.crismani@gmail.com>
2025-06-11 14:24:33 +0200

    Better lint JS and never format php on sandboxes

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -14,6 +14,18 @@ return {
       },
     },
     config = function()
+      local formatters_by_ft = {
+        -- lua = { 'stylua' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use 'stop_after_first' to run the first available formatter from the list
+        javascript = { 'prettier', 'eslint_d', stop_after_first = true },
+        typescript = { 'prettier', 'eslint_d', stop_after_first = true },
+      }
+      if not vim.env.SSH_TTY then
+        formatters_by_ft['lua'] = { 'stylua' }
+      end
       require('conform').setup {
         notify_on_error = false,
         format_on_save = function(bufnr)
@@ -22,7 +34,7 @@ return {
           -- languages here or re-enable it for the disabled ones.
           local disable_filetypes = { c = true, cpp = true, sh = true, json = true, jsonc = true }
           if vim.env.SSH_TTY then
-            disable_filetypes = { c = true, cpp = true, sh = true, json = true, jsonc = true, lua = true }
+            disable_filetypes = { c = true, cpp = true, sh = true, json = true, jsonc = true, lua = true, php = true }
           end
           local lsp_format_opt
           if disable_filetypes[vim.bo[bufnr].filetype] then
@@ -35,15 +47,7 @@ return {
             lsp_format = lsp_format_opt,
           }
         end,
-        formatters_by_ft = {
-          -- lua = { 'stylua' },
-          -- Conform can also run multiple formatters sequentially
-          -- python = { "isort", "black" },
-          --
-          -- You can use 'stop_after_first' to run the first available formatter from the list
-          javascript = { 'prettier', 'eslint_d', stop_after_first = true },
-          typescript = { 'prettier', 'eslint_d', stop_after_first = true },
-        },
+        formatters_by_ft = formatters_by_ft,
       }
     end,
   },
